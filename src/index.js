5d488eb8c80b7bd24febdd9df48f73f52f6a4148
SamG <sdgluck@gmail.com>
2016-02-12 19:24:40 +0000

    Small refactor and comment corrections

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 'use strict';
 
 /**
- * Validate arguments given to `catchAndMatch`.
+ * Validate the arguments that are given to `catchAndMatch`.
+ * Return an Error when one of these arguments is not of an expected type.
  * @param {Function} fn
  * @param {Object} matcher
  * @param {Function|undefined} cb
+ * @return {Error}
  */
-function validateArguments (fn, matcher, cb) {
+function isIllegalArguments ({ fn, matcher, cb }) {
     if (typeof fn !== 'function') {
-        throw new Error('fn should be a function');
+        return new Error('Expected fn to be a Function');
+    }
+    if (typeof cb !== 'function') {
+        return new Error('Expected cb to be a Function');
     }
     if (
         !(matcher instanceof RegExp) &&
         ['function', 'string'].indexOf(typeof matcher) === -1 &&
         matcher.constructor !== Error.constructor
     ) {
-        throw new Error('matcher should be a string, function, regular expression, or Error');
-    }
-    if (cb && typeof cb !== 'function') {
-        throw new Error('cb should be a function');
+        return new Error('Expected matcher to be a String/Function/RegExp/Error');
     }
 }
 
 /**
- * Determine if the given error satisfies the matcher defined by `_matcher`.
- * @param {Object} _matcher matcher
+ * Determine if the given `err` satisfies the criteria defined by `matcher`:
+ *  - if `matcher` is a String, match the error's message against it as a regular expression.
+ *  - if `matcher` is a Function, call the function with `err` and return the result.
+ *  - if `matcher` is an Error, expect their constructors to be the same.
+ * @param {Object} matcher matcher
  * @param {Error} err error to match against
  * @returns {Boolean}
  */
-function doesMatch (_matcher, err) {
-    const matcher = typeof _matcher === 'string' ?
-        new RegExp(_matcher) :
-        _matcher;
-    if (matcher instanceof RegExp) {
-        return matcher.test(err.message);
+function doesMatch ({ matcher, err }) {
+    if (typeof matcher === 'string') {
+        return new RegExp(matcher).test(err.message);
     }
     else if (typeof matcher === 'function') {
         return matcher(err);
@@ -45,33 +47,31 @@ function doesMatch (_matcher, err) {
 }
 
 /**
- * Invoke a function within a try block expecting an error to be thrown
- * and match the error message against a regular expression. The function returns a
- * Promise on success or you can pass it an optional callback if your test suite uses
- * callbacks to accommodate asynchronicity.
+ * Invoke a function expecting an error to be thrown.
+ * If an error is not thrown, call `cb` with an error and return a rejected Promise.
  * @param {Function} fn function to execute within a try/catch
  * @param {RegExp|String|Function|Error} matcher to match error against
  * @param {Function} [cb] callback to invoke on success
- * @returns {Promise|undefined} returns resolved Promise on success
+ * @returns {Promise} returns resolved Promise on success
  * @throws Error when `fn` does not throw or error does not satisfy `matcher`
  */
 function catchAndMatch (fn, matcher, cb) {
-
-    validateArguments(fn, matcher, cb);
-
-    return Promise
-        .resolve()
-        .then(fn)
+    return isIllegalArguments({ fn, matcher, cb })
+        .then(fn, err => {
+            throw err;
+        })
         .then(() => {
-            const error = new Error('no error thrown');
+            // If we got here the function did not throw an error
+            const error = new Error('No error thrown');
             if (cb) cb(error);
             return Promise.reject(error);
         }, (err) => {
-            if (doesMatch(matcher, err)) {
+            // Error was thrown - check that it satisfies the matcher
+            if (doesMatch({ matcher, err })) {
                 if (cb) cb();
                 return Promise.resolve();
             }
-            const error = new Error('error does not satisfy matcher');
+            const error = new Error('Error does not satisfy matcher');
             if (cb) cb(error);
             return Promise.reject(error);
         });
@@ -81,12 +81,13 @@ function catchAndMatch (fn, matcher, cb) {
 if (typeof define === 'function' && define.amd) {
     define('catchAndMatch', catchAndMatch);
 }
-else if (typeof module === 'object' && module.exports) {
+else if (typeof exports === 'object') {
     module.exports = catchAndMatch;
 }
 else if (typeof window !== 'undefined') {
     window.catchAndMatch = catchAndMatch;
 }
 else {
-    throw new Error('Environment is not supported. Please raise an issue (github.com/sdgluck/catch-and-match/issues)');
+    const issuesUrl = 'https://github.com/sdgluck/catch-and-match/issues';
+    throw new Error(`Environment is not supported. Please raise an issue (${issuesUrl})`);
 }
